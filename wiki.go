3021d0a7b63732448011b124d7c3a68641204c7a
dave <davidmasq@gmail.com>
2023-06-13 20:46:18 -0700

    lots of changes. implemented database, fixed some errors??

--- wiki.go
+++ wiki.go
@@ -1,3 +1,7 @@
+/* things to do:
+add search feature?
+*/
+
 package main
 
 import (
@@ -7,48 +11,72 @@ import (
     "os"
     "fmt"
     "log"
+    "strings"
     "net/http"
+
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+    
 )
 
-type Data struct {
+type Data struct { //data to fill out templates
     Files []string
 }
 
-type Page struct {
+type Page struct { //wiki page
     Title string
     Body []byte
 }
 
 //globals
-var templates = template.Must(template.ParseFiles("edit.html", "view.html", "index.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view|delete)/([a-zA-Z0-9]+)$")
+var templates = template.Must(template.ParseFiles("edit.html", "view.html", "index.html", "newpage.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view|delete|new)/?([a-zA-Z0-9_]+)?$")
+var validTitle = regexp.MustCompile("^([a-zA-Z0-9_]+)$")
+var db *sql.DB
 
 //page functions
-func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return os.WriteFile("pages/" + filename, p.Body, 0600)
+func getPage(name string) (*Page, error) {
+    var title []byte
+    var body []byte
+
+    row := db.QueryRow("SELECT title, content FROM pages WHERE title = ?", name)
+    err := row.Scan(&title, &body)
+    if err != nil {
+        return &Page{}, err
+    }
+
+    return &Page{Title: string(title), Body: body}, nil
 }
 
-func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := os.ReadFile("pages/" + filename)
+func (p *Page) save() error {
+    p.Title = strings.ReplaceAll(p.Title, " ", "_")
+    _, err := db.Exec("INSERT INTO pages (title, content) VALUES (?, ?)", p.Title, p.Body)
     if err != nil {
-        return nil, err
+        db.Exec("UPDATE pages SET content = ? WHERE title = ?", p.Body, p.Title)
+        return nil
     }
-    return &Page{Title: title, Body: body}, nil
+    return nil
 }
 
 //misc functions?
 func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
     m := validPath.FindStringSubmatch(r.URL.Path)
     if m == nil {
-        http.NotFound(w, r)
+        //http.NotFound(w, r)
         return "", errors.New("invalid page title")
     }
     return m[2], nil
 }
+func validateTitle(s string) error {
+    if validTitle.Match([]byte(s)) {
+        return nil
+    }
+    return fmt.Errorf("invalid")
+}
+
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    //p.Title = strings.ReplaceAll(p.Title, "_", " ")
     err := templates.ExecuteTemplate(w, tmpl+".html", p)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -60,9 +88,10 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     title, err := getTitle(w, r)
     if err != nil {
+        http.NotFound(w, r)
         return
     }
-    p, err := loadPage(title)
+    p, err := getPage(title)
     if err != nil {
         http.Redirect(w, r, "/edit/"+title, http.StatusFound)
         return
@@ -75,9 +104,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         return
     }
-    p, err := loadPage(title)
+    if title == "" {
+        renderTemplate(w, "newpage", &Page{})
+        return
+    }
+    p, err := getPage(title)
     if err != nil {
-        p = &Page{Title: title}
+        p.Title = title
     }
     renderTemplate(w, "edit", p)
 }
@@ -87,38 +120,57 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         return
     }
+    if title == "" {
+        title = strings.ReplaceAll(r.FormValue("title"), " ", "_")
+        err := validateTitle(title)
+        if err != nil {
+            http.NotFound(w, r)
+            return
+        }
+    }
+    
     body := r.FormValue("body")
     p := &Page{Title: title, Body: []byte(body)}
     err = p.save()
     if err != nil {
+        log.Fatal(err)
+        /*
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
+        */
     }
-    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+    http.Redirect(w, r, "/view/"+p.Title, http.StatusFound)
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
     title, err := getTitle(w, r)
     if err != nil {
-        return
+        a := r.FormValue("title")
+        if a == "" {
+            http.NotFound(w, r)
+            return
+        }
+        title = a
     }
-    err = os.Remove("pages/" + title + ".txt")
+    _ , err = db.Exec("DELETE FROM pages WHERE title = ?", title)
     if err != nil {
         log.Fatal(err)
     }
     http.Redirect(w, r, "/", http.StatusFound)
 }
-    
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    files, err := os.ReadDir("pages/")
+    rows, err := db.Query("SELECT title FROM pages")
     if err != nil {
         log.Fatal(err)
     }
     names := Data{}
-    for _, file := range files {
-        names.Files = append(names.Files, file.Name()[:len(file.Name())-4])
+    for rows.Next() {
+        var title string
+        rows.Scan(&title)
+        names.Files = append(names.Files, title)
     }
+
     err = templates.ExecuteTemplate(w, "index.html", names)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -128,9 +180,19 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     fmt.Println("starting up")
+    pass := os.Getenv("db_pass")
+    var err error
+    db, err = sql.Open("mysql", "dave:" + pass + "@tcp(127.0.0.1:3306)/wiki")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+    fmt.Println("database connected")
+
 
     http.HandleFunc("/view/", viewHandler)
     http.HandleFunc("/edit/", editHandler)
+    http.HandleFunc("/new/", editHandler)
     http.HandleFunc("/save/", saveHandler)
     http.HandleFunc("/delete/", deleteHandler)
     http.HandleFunc("/", indexHandler)
